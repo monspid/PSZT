8e4a21c2d27b661fcf2f8fe1ba1c1a2c3cfae261
monspid <monspid@gmail.com>
2017-01-05 14:14:35 +0100

    usunięcie dziwnych wcięć
    Zamiana 8 spacji na 1 tab.

--- Individual/Individual.py
+++ Individual/Individual.py
@@ -2,41 +2,40 @@ import copy
 import random
 
 class Individual:
-    def __init__(self, args, dist):
-        self.__arguments = args.copy()
-        self.__distributions = dist.copy()
-        self.__value = 0.0
+	def __init__(self, args, dist):
+		self.__arguments = args.copy()
+		self.__distributions = dist.copy()
+		self.__value = 0.0
 
-    def __str__(self):
-        return "arguments: " + str(self.__arguments) + "\ndistributions: " + str(self.__distributions) + "\nvalue: " + str(self.__value) + "\n"
+	def __str__(self):
+		return "arguments: {}\n distributions: {}\n value: {}\n".format(self.__arguments, self.__distributions, self.__value)
 
-    def __repr__(self):
-        return self.__str__()
+	def __repr__(self):
+		return self.__str__()
 
-    def randomize(self):
-        for x in self.__arguments.keys():
-            self.__arguments[x] =  random.uniform(-100, 100)
-            self.__distributions[x] =  random.uniform(-5, 5)
+	def randomize(self):
+		for x in self.__arguments.keys():
+			self.__arguments[x] = random.uniform(-100, 100)
+			self.__distributions[x] = random.uniform(-5, 5)
 
-    def __init_distributions(self):
-        for x in self.__distributions.keys():
-            self.__distributions[x] =  random.uniform(-5, 5)
+	def __init_distributions(self):
+		for x in self.__distributions.keys():
+			self.__distributions[x] = random.uniform(-5, 5)
 
-    def get_arguments(self):
-        return copy.deepcopy(self.__arguments)
-    
-    def set_arguments(self, new_variables):
-        self.__variables = new_variables.copy()
+	def get_arguments(self):
+		return copy.deepcopy(self.__arguments)
+	
+	def set_arguments(self, new_variables):
+		self.__variables = new_variables.copy()
 
-    def get_distributions(self):
-        return self.__distributions.copy()
+	def get_distributions(self):
+		return self.__distributions.copy()
 
-    def set_distributions(self, new_variables):
-        self.__distributions = new_variables.copy()
+	def set_distributions(self, new_variables):
+		self.__distributions = new_variables.copy()
 
-    def get_value(self):
-        return self.__value
+	def get_value(self):
+		return self.__value
 
-    def set_value(self, new_value):
-        self.__value = new_value
-
+	def set_value(self, new_value):
+		self.__value = new_value
